a66ff16e2ec473574607fc9ee53a211a157e87c1
Enrique Jimenez <ejimenez2292@gmail.com>
2022-08-07 19:40:07 -0500

    converting to react app

---
+++ anyandeverything/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
